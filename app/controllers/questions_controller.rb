fcd2db465a557227215157ebbd883ef5897e8514
Guilherme Silva <gfsilva@hotmail.com>
2021-10-31 08:41:44 -0300

    revisado

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.ends_with?("?")
+      @answer = "Don't ask me stuff"
+    elsif
+      @answer = "Great"
+    end
+  end
+
 end
